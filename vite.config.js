ea90ec66c3d4119a73979c5c892c4a16a7749a7c
Kwangmin Chung <gm.jung9912@gmail.com>
2026-09-25 15:07:51 +0900

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 상대 경로로 빌드해 dist/index.html을 서버 없이 파일로 직접 열 수 있게 한다.
-  base: './',
+  base: '/My_Project/',
 })
